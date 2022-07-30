737bd2d4d6c70ac96f8ee3fadbb4ec071a8be7a5
mpenajoia <penajoia.marco@gmail.com>
2022-07-29 20:49:49 -0400

    Status component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Content from "./components/Content";
 import Recent from "./components/Recent";
 import TopBar from "./components/TopBar";
 
@@ -7,6 +8,8 @@ function App() {
   return (
     <div className="w-full flex flex-col items-center">
       <TopBar />
+      <h1 className="text-6xl font-bold my-4">Kapybara Token IDO</h1>
+      <Content />
       <div className="w-[1000px] flex flex-col items-center">
         <Recent />
       </div>
---
+++ src/components/Content.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Status from './Status'
+
+function Content() {
+  return (
+    <div className='w-[1100px]'>
+        <Status /> 
+    </div>
+  )
+}
+
+export default Content
